51ed542f703804f2ddefb6c95c0bee9ee29ea009
John Denny <johnnydenny@gmail.com>
2023-04-21 21:48:16 -0400

    Inline svg instead of next image

--- components/ColorCards/index.tsx
+++ components/ColorCards/index.tsx
@@ -19,7 +19,7 @@ export default function ColorCards({ colors }: { colors: Color[] }) {
   return (
     <>
       {colors.map((color) => (
-        <div
+        <button
           key={color.id}
           className={styles.card}
           onClick={() => copyHexadecimal(color.hexadecimal, color.id)}
@@ -31,17 +31,20 @@ export default function ColorCards({ colors }: { colors: Color[] }) {
           >
             {copiedId === color.id && (
               <div className={color_styles.icon}>
-                <Image
-                  alt="copied"
-                  src="./check-circle-fill.svg"
-                  width={24}
-                  height={24}
-                ></Image>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="grey"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                </svg>
               </div>
             )}
           </div>
           <h1 className={styles.header}>{color.hexadecimal}</h1>
-        </div>
+        </button>
       ))}
     </>
   );
